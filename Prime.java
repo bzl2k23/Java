2ada143a4997d53bb8deb9a6ecf44da27e5c627d
bzl2k23 <155909278+bzl2k23@users.noreply.github.com>
2024-01-08 00:23:44 +0530

    Packages-pack

---
+++ Prime.java
@@ -0,0 +1,15 @@
+package pack;
+
+public class Prime {
+    public int PrimeCheck(int x) {
+        int flag = 0;
+        for (int i = 2; i <= Math.sqrt(x); i++) {
+            if (x % i == 0) {
+                System.out.println(x + " is not a prime");
+                flag = 1;
+                break;
+            }
+        }
+        return (flag);
+    }
+}
